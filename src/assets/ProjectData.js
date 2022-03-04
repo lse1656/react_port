27616f19c519c021543ad6391fb21c5fe96847c2
lse1656 <lse1656@naver.com>
2022-03-04 18:26:48 +0900

    project data 다른 파일로 분리

---
+++ src/assets/ProjectData.js
@@ -0,0 +1,44 @@
+const ProjectData = [
+  {
+    id : 1,
+    title : "RESPONSIVE WEBSITE",
+    detail : {
+      projectTitle : "GOV24",
+      type : "CLONE CODING",
+      day : 3,
+      skill : "HTML, CSS, JAVASCRIPT",
+    }
+  },
+  {
+    id : 2,
+    title : "DESKTOP WEBSITE",
+    detail : {
+      projectTitle : "GOV24",
+      type : "CLONE CODING",
+      day : 3,
+      skill : "HTML, CSS, JAVASCRIPT",
+    }
+  },
+  {
+    id : 3,
+    title : "SASS WEBSITE",
+    detail : {
+      projectTitle : "GOV24",
+      type : "CLONE CODING",
+      day : 3,
+      skill : "HTML, CSS, JAVASCRIPT",
+    }
+  },
+  {
+    id : 4,
+    title : "REACT PORTFOLIO",
+    detail : {
+      projectTitle : "GOV24",
+      type : "CLONE CODING",
+      day : 3,
+      skill : "HTML, CSS, JAVASCRIPT",
+    }
+  }
+]
+
+export default ProjectData;
